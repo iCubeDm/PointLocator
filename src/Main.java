1b74fa28f6210824c3a9f0049bd9f6b1a7068db1
dymsp <dymsp@yandex-team.ru>
2014-12-10 08:27:00 +0300

    [Core] core classes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import ru.locator.core.IntersectionHelper;
+import ru.locator.core.Point;
+import ru.locator.core.Segment;
+
 /**
  * Created by dymsp
  */
@@ -5,5 +9,15 @@ public class Main
 {
 		public static void main(String[] args)
 		{
+				Point A = new Point(-5, 0);
+				Point B = new Point(5, 0);
+				Point C = new Point(0, -5);
+				Point D = new Point(0, 5);
+
+				Segment AB = new Segment(A, B);
+				Segment CD = new Segment(C, D);
+
+
+				System.out.println(IntersectionHelper.isIntersection(AB, CD));
 		}
 }
